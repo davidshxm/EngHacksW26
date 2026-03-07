b0380a2929ee3b8f5bb0b526459cde1c1c555380
justin <j537zhou@uwaterloo.ca>
2026-03-07 14:31:36 -0500

    created main

---
+++ main/main.ino
@@ -0,0 +1,13 @@
+// IR Sensors
+const int irSensorPin1 = A0;
+const int irSensorPin2 = A1;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
